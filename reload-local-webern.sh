a73411acc7b671685f69011081ad796c2bb534ef
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-01-16 11:10:57 +0100

    edit

---
+++ reload-local-webern.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# execute this script with 
+# chmod +x reload-local-webern.sh
+# ./reload-local-webern.sh
+
+# e: exit on error
+# u: treat unset variables as error
+# o pipefail: if one command in a pipe fails, let the pipe fail
+set -euo pipefail
+
+# debugging mode: print every command before execution
+set -x
+
+
+# restart the stack
+uvx dsp-tools stop-stack
+uvx dsp-tools start-stack --no-prune
+
+# load the necessary data into the stack
+curl --request PUT \
+http://localhost:3030/knora-test/data?graph=http%3A%2F%2Fwww.knora.org%2Fdata%2Fadmin \
+--header 'Content-Type: text/turtle;encoding=UTF-8' \
+-u "admin:test" \
+-T ~/Desktop/knora-admin-data.ttl
+
+curl --request PUT \
+http://localhost:3030/knora-test/data?graph=http%3A%2F%2Fwww.knora.org%2Fontology%2F0806%2Fwebern-onto \
+--header 'Content-Type: text/turtle;encoding=UTF-8' \
+-u "admin:test" \
+-T ~/Desktop/webern-onto.ttl
+
+curl --request PUT \
+http://localhost:3030/knora-test/data?graph=http%3A%2F%2Fwww.knora.org%2Fdata%2F0806%2Fwebern \
+--header 'Content-Type: text/turtle;encoding=UTF-8' \
+-u "admin:test" \
+-T ~/Desktop/webern-data.ttl
+
+
+# restart the api container
+docker stop start-stack-api-1
+docker start start-stack-api-1
