a7ffb47f9a0accd01bd9c503257f811f11cd858e
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-01-23 14:48:08 +0100

    edit

---
+++ webern-delete-resource/1_extract-webern-from-prod.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# execute this script with
+# chmod +x ./path/to/script.sh
+# ./path/to/script.sh ${DB_PASSWORD}
+
+# e: exit on error
+# u: treat unset variables as error
+# o pipefail: if one command in a pipe fails, let the pipe fail
+set -euo pipefail
+
+# debugging mode: print every command before execution
+set -x
+
+DB_PASSWORD=$1
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+declare -A graphs
+graphs["http%3A%2F%2Fwww.knora.org%2Fdata%2Fadmin"]="knora-admin-data.ttl"
+graphs["http%3A%2F%2Fwww.knora.org%2Fontology%2F0806%2Fwebern-onto"]="webern-onto.ttl"
+graphs["http%3A%2F%2Fwww.knora.org%2Fdata%2F0806%2Fwebern"]="webern-data.ttl"
+
+for graph in "${!graphs[@]}"; do
+    dest=${graphs[$graph]}
+    curl "https://db.dasch.swiss/dsp-repo/data?graph=${graph}" \
+        --header "Accept: text/turtle;encoding=UTF-8" \
+        -u "admin:${DB_PASSWORD}" \
+        > "$SCRIPT_DIR/$dest"
+done
